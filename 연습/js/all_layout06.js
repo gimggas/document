20d7a623293ee6327b66182a426add07ead8e4af
gimggas <98053705+gimggas@users.noreply.github.com>
2022-03-18 18:11:03 +0900

    Add files via upload

---
+++ 연습/js/all_layout06.js
@@ -0,0 +1,35 @@
+$(function(){
+    //////////////////////////////////
+    
+    // topBannner : lee 2022.03.17 수정
+    function topBannerHandler() {
+        // $('.topBannner').hide(); //display:none;
+        $('.topBannner').slideUp();
+    }
+    $('.topBannner .container i').on('click', topBannerHandler);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //////////////////////////////////
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////
